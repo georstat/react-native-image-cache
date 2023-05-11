3288507f464a30a8e9f1a4a7364aae88e5fb4007
Efstathios Ntonas <stathis@hikiapp.com>
2023-05-12 01:05:20 +0300

    chore: prettified files

--- src/CacheManager.ts
+++ src/CacheManager.ts
@@ -103,7 +103,11 @@ export default class CacheManager {
     noCache?: boolean,
     maxAge?: number
   ): CacheEntry {
-    if (!CacheManager.entries[source] || CacheManager.entries[source].options?.headers?.Authorization !== options?.headers?.Authorization) {
+    if (
+      !CacheManager.entries[source] ||
+      CacheManager.entries[source].options?.headers?.Authorization !==
+        options?.headers?.Authorization
+    ) {
       CacheManager.entries[source] = new CacheEntry(
         source,
         options,
