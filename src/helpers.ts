f272a2838ab7c4836b2d1d7783f0e5ef7e9dc003
saif <sabueideh@scriptechn.com>
2023-09-03 14:35:19 +0300

    fix

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@ import { Platform } from "react-native";
 
 export function isRemoteImage(url){
   // Check if the URL starts with 'http://' or 'https://' and not a number (using Require method)
-  if (url && !isImageWithRequire(url) && (url.startsWith('http://') || url.startsWith('https://'))) {
+  if (url && !isImageWithRequire(url) && (url?.startsWith('http://') || url?.startsWith('https://'))) {
     return true; // Remote image
   }
   return false;
